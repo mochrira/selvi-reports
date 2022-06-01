6f60d9d2b0fcc853582b3db33b1b35ec7fc0453d
mochrira <mochrira@gmail.com>
2022-06-01 14:47:29 +0700

    New Version Arrives

---
+++ README.md
@@ -0,0 +1,70 @@
+# Selvi PDF
+
+Sebuah library untuk membuat laporan pdf yang dibuat menggunakan TCPDF mengadopsi sistem Band dalam pembuatan laporan yang sudah lama diperkenalkan oleh FastReport&copy;.
+
+## Instalasi
+
+```
+composer require mochrira/selvi-pdf
+```
+
+## Memulai
+
+```
+<?php 
+    require 'vendor/autoload.php';
+
+    $pdf = new \Selvi\Pdf();
+    $pdf->pageStart([
+        'margins' => ['bottom' => .5, 'top' => .5]
+    ]);
+
+    $pdf->pageHeader(function ($pdf) {
+        $pdf->rowStart();
+        $pdf->column('Page Header');
+        $pdf->rowEnd();
+    });
+
+    $pdf->band(function ($pdf) {
+        $pdf->rowStart();
+        $pdf->column('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', ['width' => '50%', 'multiline' => false, 'border' => 1]);
+        $pdf->column('Halo', ['width' => '50%']);
+        $pdf->rowEnd();
+    });
+
+    $pdf->pageFooter(function ($pdf) {
+        $pdf->rowStart();
+        $pdf->column('Page '.$pdf->PageNo());
+        $pdf->rowEnd();
+    });
+?>
+```
+
+## Master Band
+
+```
+...
+
+$pdf->masterStart();
+    $pdf->masterHeader(function ($pdf) {
+        $pdf->rowStart();
+        $pdf->column('Master Header 1');
+        $pdf->rowEnd();
+    });
+        for($i=1; $i<=125; $i++) {
+            $pdf->masterBand(function ($pdf) use ($i) {
+                $pdf->rowStart();
+                $pdf->column('Master Band 1 - '.$i);
+                $pdf->rowEnd();
+            });
+        }
+    $pdf->band(function ($pdf) {
+        $pdf->rowStart();
+        $pdf->column('Master Footer uses generic band'.$i);
+        $pdf->rowEnd();
+        $pdf->Ln();
+    });
+$pdf->masterEnd();
+
+...
+```
--- src/Pdf.php
+++ src/Pdf.php
@@ -6,48 +6,242 @@ use TCPDF;
 
 class Pdf extends TCPDF {
 
+    private $init = false;
+    private $components = [];
+
+    private $simulationLock = false;
+    private $simulation = false;
+
     function __construct() {}
 
-    private $init = false;
-    private $pageArgs = ['orientation' => 'P', 'units' => 'in', 'size' => 'A4'];
+    function startSimulation($lock = false) {
+        if($this->simulation === false) {
+            $this->simulation = true;
+            $this->simulationLock = $lock;
+            $this->startTransaction();
+        }
+    }
+
+    function stopSimulation($forceStop = false) {
+        if(($this->simulation && !$this->simulationLock) || $forceStop) {
+            $this->simulation = false;
+            $this->simulationLock = false;
+            $this->rollbackTransaction(true);
+        }
+    }
+
+    function getNearestTopIndex($name, $startIndex, $stopIndex = 0) {
+        $i = $startIndex;
+        while($i >= $stopIndex && $i >= 0) {
+            if($this->components[$i]['name'] == $name) {
+                return $i;
+            }
+            if($this->components[$i]['name'] == 'pageStart') {
+                return null;
+            }
+            $i--;
+        }
+        return -1;
+    }
+
+    function getNearestTop($name, $startIndex, $stopIndex = 0) {
+        $index = $this->getNearestTopIndex($name, $startIndex, $stopIndex);
+        if($index > -1) return $this->components[$index];
+        return null;
+    }
+
+    function getNearestBottomIndex($name, $startIndex, $stopIndex = 0) {
+        $i = $startIndex;
+        if($stopIndex == 0) $stopIndex = count($this->components) - 1;
+        while($i <= $stopIndex) {
+            if($this->components[$i]['name'] == $name) {
+                return $i;
+            }
+            if($this->components[$i]['name'] == 'pageEnd') {
+                return null;
+            }
+            $i++;
+        }
+        return -1;
+    }
+
+    function getNearestBottom($name, $startIndex, $stopIndex = 0) {
+        $index = $this->getNearestBottomIndex($name, $startIndex, $stopIndex);
+        if($index > -1) return $this->components[$index];
+        return null;
+    }
 
     function pageStart($args = []) {
-        $this->pageArgs = array_merge($this->pageArgs, $args);
+        $this->components[] = [
+            'name' => 'pageStart',
+            'args' => [$args]
+        ];
+    }
+
+    function _pageStart($args = [], $comIndex = null) {
+        $default = ['orientation' => 'P', 'units' => 'in', 'size' => 'A4'];
+        $pageArgs = array_merge($default, $args);
         if(!$this->init) {
-            parent::__construct($this->pageArgs['orientation'], $this->pageArgs['units'], $this->pageArgs['size']);
+            parent::__construct($pageArgs['orientation'], $pageArgs['units'], $pageArgs['size']);
+            $this->SetPrintHeader(false);
+            $this->SetPrintFooter(false);
             $this->init = true;
         }
 
-        if($this->headerCallback !== null) {
-            $this->SetPrintHeader(true);
-        }
-        if(isset($this->pageArgs['margins'])) {
-            $this->SetMargins(...$this->pageArgs['margins']);
-        }
+        $this->SetLeftMargin($pageArgs['margins']['left'] ?? 0.5);
+        $this->SetRightMargin($pageArgs['margins']['right'] ?? 0.5);
+        $this->SetTopMargin(0);
+        $this->components[$comIndex]['margins']['top'] = $pageArgs['margins']['top'] ?? 0;
+        $this->components[$comIndex]['margins']['bottom'] = $pageArgs['margins']['bottom'] ?? 0;
 
         $this->setAutoPageBreak(false);
-        $this->AddPage($this->pageArgs['orientation'], $this->pageArgs['size']);
+        $this->AddPage($pageArgs['orientation'], $pageArgs['size']);
+        $this->SetY(0);
+        $this->_pageHeader($comIndex);
     }
 
-    function pageEnd() {
+    function pageEnd($args = []) {
+        $this->components[] = [
+            'name' => 'pageEnd',
+            'args' => $args
+        ];
+    }
+
+    function _pageEnd($comIndex = null) {
+        $this->_pageFooter($comIndex);
         $this->endPage();
     }
 
-    private $headerCallback;
-    private $onHeader = false;
-
-    function pageHeader($callback) {
-        $this->headerCallback = $callback;
+    function pageHeader($callback = null) {
+        $this->components[] = [
+            'name' => 'pageHeader',
+            'callback' => $callback
+        ];
     }
 
-    public function Header() {
-        $this->onHeader = true;
-        call_user_func($this->headerCallback);
-        $this->onHeader = false;
+    function _pageHeader($comIndex) {
+        $pageHeader = $this->getNearestTop('pageHeader', $comIndex);
+        if(!$pageHeader) {
+            $pageHeader = $this->getNearestBottom('pageHeader', $comIndex);
+        }
+
+        if($pageHeader) {
+            $pageHeader['callback']($this);
+            $this->Ln();
+        }
+
+        $pageStart = $this->getNearestTop('pageStart', $comIndex);
+        $this->SetY($pageStart['margins']['top']);
+    }
+
+    function pageFooter($callback = null) {
+        $this->components[] = [
+            'name' => 'pageFooter',
+            'callback' => $callback
+        ];
+    }
+
+    function _pageFooter($comIndex = null) {
+        $pageFooter = $this->getNearestTop('pageFooter', $comIndex);
+        if(!$pageFooter) {
+            $pageFooter = $this->getNearestBottom('pageFooter', $comIndex);
+        }
+
+        if($pageFooter) {
+            $pageStart = $this->getNearestTop('pageStart', $comIndex);
+            $y = $this->getPageHeight() - $pageStart['margins']['bottom'];
+            $this->SetY($y);
+            $pageFooter['callback']($this);
+            $this->Ln();
+        }
+    }
+
+    function band($callback = null) {
+        $this->components[] = [
+            'name' => 'band',
+            'args' => [$callback]
+        ];
+    }
+
+    function _band($callback = null, $comIndex = null) {
+        if($callback != null) {
+            $this->_detectPageBreak($callback, $comIndex);
+            $callback($this);
+        }
+    }
+
+    function masterStart() {
+        $this->components[] = [
+            'name' => 'masterStart'
+        ];
+    }
+
+    function masterHeader($callback = null) {
+        $this->components[] = [
+            'name' => 'masterHeader', 
+            'args' => [$callback]
+        ];
+    }
+
+    function _masterHeader($callback = null, $comIndex = null) {
+        if($callback != null) {
+            $callback($this);
+        }
+    }
+
+    function masterBand($callback = null) {
+        $this->components[] = [
+            'name' => 'masterBand',
+            'args' => [$callback]
+        ];
+    }
+
+    function _masterBand($callback = null, $comIndex = null) {
+        if($callback != null) {
+            $pageBreak = $this->_detectPageBreak($callback, $comIndex);
+            $masterStartIndex = $this->getNearestTopIndex('masterStart', $comIndex);
+            $masterHeader = $this->getNearestTop('masterHeader', $comIndex, $masterStartIndex);
+            if($pageBreak && $masterHeader != null) {
+                $masterHeader['args'][0]($this);
+            }
+
+            $callback($this);
+        }
+    }
+
+    function masterEnd() {
+        $this->components[] = [
+            'name' => 'masterEnd'
+        ];
+    }
+
+    function _detectPageBreak($callback, $comIndex) {
+        $isBreak = false;
+
+        $this->startSimulation(true /** lock simulation */);
+
+        $y = $this->GetY();
+        if($callback != null) {
+            $callback($this);
+            $this->Ln();
+        }
+        $height = $this->GetY() - $y;
+
+        $this->stopSimulation(true /** force unlock simulation */);
+
+        $pageStart = $this->getNearestTop('pageStart', $comIndex);
+        $pageHeight = $this->getPageHeight();
+        if(($y + $height) > ($pageHeight - $pageStart['margins']['bottom'])) {
+            $this->_pageFooter($comIndex);
+            $this->AddPage();
+            $this->_pageHeader($comIndex);
+            $isBreak = true;
+        }
+
+        return $isBreak;
     }
 
     private $cols = [];
-    private $simulate = true;
 
     function percentWidth($percent) {
         $margins = $this->getMargins();
@@ -56,65 +250,74 @@ class Pdf extends TCPDF {
         return (float)$percent / 100 * $innerWidth;
     }
 
-    function rowStart() {
-        // start simulation
-        $this->simulate = true;
-        $this->cols = [];
-        $this->startTransaction();
+    function getPageInnerWidth() {
+        $margins = $this->getMargins();
+        $width = $this->getPageWidth();
+        return $width - $margins['left'] - $margins['right'];
     }
 
-    function column($txt, $options, $break = false) {
-        // merge options
+    function rowStart() {
+        $this->cols = [];
+        $this->startSimulation();
+    }
+
+    function column($txt, $options = [], $break = false) {
         $options = array_merge([
+            'width' => 0,
             'height' => 0,
             'align' => 'L',
             'valign' => 'M',
             'multiline' => false,
-            'border' => 1
+            'border' => 0,
+            'fill' => 0
         ], $options);
 
-        // decide multiline or not
+        if($this->simulation && !$this->simulationLock) { 
+            $options['width'] = (strpos($options['width'], '%') === false ? ($options['width'] == 0 ? $this->getPageInnerWidth() : $options['width']) : $this->percentWidth($options['width']));
+        }
+
         if($options['multiline'] == true) {
             $this->MultiCell(
-                (strpos($options['width'], '%') === false ? $options['width'] : $this->percentWidth($options['width'])), // width
+                $options['width'], // width
                 $options['height'], // height
                 $txt, // content
                 $options['border'], // border
                 $options['align'], 
-                0, // fill
+                $options['fill'], // fill
                 $break ? 1 : 0 // break
             );
+            if($this->simulation && !$this->simulationLock) $options['height'] = $this->GetLastH();
         } else {
+            if(!$this->simulation) {
+                $this->StartTransform();
+                $this->Rect($this->GetX(), $this->GetY(), $options['width'], $options['height'], 'CNZ', 'LTRB');
+            }
             $this->Cell(
-                (strpos($options['width'], '%') === false ? $options['width'] : $this->percentWidth($options['width'])), // width
+                $options['width'], // width
                 $options['height'], // height
                 $txt, // content
                 $options['border'], // border
                 $break ? 1 : 0, // break
                 $options['align'], // align
-                0, // fill
+                $options['fill'], // fill
                 '',  // link
                 0, // stretch
                 false, // ignore_min_height
                 'T', // calign
                 $options['valign'] // valign
             );
+            if($this->simulation && !$this->simulationLock) $options['height'] = $this->GetLastH(); 
+            if(!$this->simulation) {
+                $this->StopTransform();   
+            }
         }
 
-        // if simulate, save the options
-        if($this->simulate) {
-            $this->cols[] = [
-                $txt, 
-                // save dynamic height, so all cols have same height as maximum height
-                array_merge($options, [
-                    'height' => $options['height'] == 0 ? $this->GetLastH() : $options['height']
-                ]), 
-                $break];
+        if($this->simulation && !$this->simulationLock) {
+            $this->cols[] = [$txt, $options, $break];
         }
     }
 
     function rowEnd() {
-        // find the heightest column
         $heightest = 0;
         foreach($this->cols as $col) {
             if($col[1]['height'] > $heightest) {
@@ -122,44 +325,31 @@ class Pdf extends TCPDF {
             }
         }
 
-        // rollback simulation
-        $this->rollbackTransaction(true);
+        $this->stopSimulation();
 
-        // if not printing header, detect page for page break
-        if($this->onHeader === false) {
-            $this->detectPageBreak($heightest);
-        }
-        
-        // display immediately
-        $this->simulate = false;
-        foreach($this->cols as $index => $col) {
-            $col[1]['height'] = $heightest;
-            if($index == count($this->cols) - 1) {
-                $col[2] = true;
+        if(!$this->simulation) {
+            foreach($this->cols as $index => $col) {
+                $args = $col;
+                $args[1]['height'] = $heightest;
+                if($index == count($this->cols) - 1) {
+                    $args[2] = 1;
+                }
+                $this->column(...$args);
             }
-            $this->column(...$col);
         }
     }
 
-    function detectPageBreak($rowHeight = 0) {
-        $height = $rowHeight;
-        if($rowHeight == 0) {
-            $height = $this->GetLastH();
+    function _render() {
+        foreach($this->components as $index => $component) {
+            if(!in_array($component['name'], ['pageHeader', 'pageFooter'])) {
+                call_user_func_array([$this, '_'.$component['name']], array_merge($component['args'], [$index]));
+            }
         }
-        if(($this->GetY() + $height) >= $this->GetPageHeight()) {
-            $tmpCols = $this->cols;
-            $this->pageBreak();
-            $this->cols = $tmpCols;
-        }
-    }
-
-    function pageBreak() {
-        $this->AddPage($this->pageArgs['orientation'], $this->pageArgs['size']);
     }
 
     function render($name = 'download.pdf') {
+        $this->_render();
         $this->Output($name, 'I');
         die();
     }
-
 }
