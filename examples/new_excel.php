513359828a03e8f4775441de38b8847ddc24f513
Kusen Abdullah <kusena322@gmail.com>
2024-10-25 16:19:59 +0700

    #6 #7 #8 init excel

--- examples/new_excel.php
+++ examples/new_excel.php
@@ -8,9 +8,36 @@ $excel = new Selvi\Report\Excel();
 $excel->pageSetup([
     'orientation' => 'portrait'
 ]);
-$excel->open('test.xlsx');
+$excel->openBrowser('test.xlsx');
 
-$excel->addRow(['Carl', 'is', 'great!']);
-$excel->addRow(['Carl']);
+$excel->setBold(true);
+$excel->setFontSize(14);
+$excel->rowStart();
+    $excel->column('LAPORAN DATA PENDAFTAR', ['colspan' => 5, 'align' => 'C']);
+$excel->rowEnd();
+$excel->restoreFontSize();
 
+$excel->setFillColor(31, 108, 142);
+$excel->setTextColor(255, 255, 255);
+
+$excel->rowStart();
+    $excel->column('3510161103920002', ['fill' => 1, 'align' => 'C']);
+    $excel->column('Fill Colored', ['fill' => 1, 'border' => 'b', 'align' => 'C']);
+    $excel->column('Fill Colored', ['fill' => 1, 'border' => 'b', 'align' => 'L']);
+    $excel->column('Fill Colored', ['fill' => 1, 'border' => 'b', 'align' => 'C']);
+    $excel->column('Fill Colored', ['fill' => 1, 'border' => 'b', 'align' => 'R']);
+$excel->rowEnd();
+$excel->clearFillColor();
+// $excel->clearTextColor();
+$excel->setBold(false);
+
+for($i = 1; $i <= 100; $i++) {
+    $excel->rowStart();
+        $excel->column('Inner Width', ['border' => 'b']);
+        $excel->column('Inner Width', ['border' => 'b']);
+        $excel->column('Inner Width', ['border' => 'b']);
+        $excel->column('Inner Width', ['border' => 'b']);
+        $excel->column('Inner Width', ['border' => 'b']);
+    $excel->rowEnd();
+}
 $excel->close();
--- src/Excel.php
+++ src/Excel.php
@@ -2,7 +2,15 @@
 
 namespace Selvi\Report;
 
+use OpenSpout\Common\Entity\Cell;
 use OpenSpout\Common\Entity\Row;
+use OpenSpout\Common\Entity\Style\Border;
+use OpenSpout\Common\Entity\Style\BorderPart;
+use OpenSpout\Common\Entity\Style\CellAlignment;
+use OpenSpout\Common\Entity\Style\CellVerticalAlignment;
+use OpenSpout\Common\Entity\Style\Color;
+use OpenSpout\Common\Entity\Style\Style;
+use OpenSpout\Reader\XLSX\Sheet;
 use OpenSpout\Writer\XLSX\Writer;
 use OpenSpout\Writer\XLSX\Options;
 use OpenSpout\Writer\XLSX\Options\PageMargin;
@@ -25,13 +33,40 @@ class Excel {
         'landscape' => PageOrientation::LANDSCAPE
     ];
 
+    private $align = [
+        'l' => CellAlignment::LEFT,
+        'c' => CellAlignment::CENTER,
+        'r' => CellAlignment::RIGHT,
+        'j' => CellAlignment::JUSTIFY
+    ];
+
+    private $valign = [
+        't' => CellVerticalAlignment::TOP,
+        'b' => CellVerticalAlignment::BOTTOM,
+        'c' => CellVerticalAlignment::CENTER,
+        'j' => CellVerticalAlignment::JUSTIFY,
+    ];
+
+    private $border = [
+        't' => Border::TOP,
+        'b' => Border::BOTTOM,
+        'l' => Border::LEFT,
+        'r' => Border::RIGHT,
+    ];
+
     private Writer $writer;
     private Options $options;
-    private $sheet;
+    private Sheet $sheet;
+    private array $cells = [];
+    private Style $style;
+
+    private $pageMargins = ['top' => .5, 'left' => .5, 'bottom' => .5, 'right' => .5];
+    private $pageOptions = ['orientation' => 'portrait', 'size' => 'A4'];
 
     function __construct() {
         $this->options = new Options();
         $this->writer = new Writer($this->options);
+        $this->style = new Style();
         // $this->sheet = $this->writer->getCurrentSheet();
     }
 
@@ -39,13 +74,7 @@ class Excel {
         return $this->writer;
     }
 
-    private $x = 0;
-    private $y = 0;
-
-    private $pageMargins = ['top' => .5, 'left' => .5, 'bottom' => .5, 'right' => .5];
-    private $pageOptions = ['orientation' => 'portrait', 'size' => 'A4'];
-
-    function pageSetup($options) {
+    function pageSetup(array $options) {
         $this->pageMargins = array_merge($this->pageMargins, $options['margin'] ?? []);
         $this->pageOptions = array_merge($this->pageOptions, $options);
         $this->options->setPageSetup(new PageSetup(
@@ -63,18 +92,88 @@ class Excel {
         ));
     }
 
-    
+    function setFontSize(int $size){
+        $this->style->setFontSize($size);
+    }
 
-    function addRow(array $data) {
-        $rowFromValues = Row::fromValues($data);
-        $this->writer->addRow($rowFromValues);
+    function restoreFontSize(){
+        $this->style->setFontSize(11);
+    }
+
+    private $fillColor = null;
+    function setFillColor($r, $g, $b) {
+        $this->fillColor = sprintf("FF%02x%02x%02x", $r, $g, $b);
+        $this->style->setBackgroundColor($this->fillColor);
+    }
+
+    private $textColor = null;
+    function setTextColor($r, $g, $b) {
+        $this->textColor = sprintf("FF%02x%02x%02x", $r, $g, $b);
+        $this->style->setFontColor($this->textColor);
+    }
+
+    public function setBold(bool $bold){
+        if ($bold) {
+            $this->style->setFontBold();
+        }
+    }
+
+    function clearFillColor(){
+        $this->style->setBackgroundColor(Color::WHITE);
+    }
+
+    function clearTextColor(){
+        $this->style->setFontColor(Color::BLACK);
+    }
+
+    public function rowStart(){
+        $this->cells = [];
+    }
+
+    private function setBorder(string $border) : Border {
+        $borerPart = new BorderPart(
+            name: $this->border[strtolower($border)],
+            color:Color::BLACK, 
+            width:Border::WIDTH_THIN, 
+            style:Border::STYLE_SOLID
+        );
+
+        return new Border($borerPart);
+    }
+
+    public function column(mixed $txt, array $options = null){
+        if (is_array($options) && count($options) > 0 ) {
+            $style = new Style();
+            if (isset($options['align'])) {
+                $style->setCellAlignment($this->align[strtolower($options['align'])]);
+            }
+
+            if (isset($options['valign'])) {
+                $style->setCellVerticalAlignment($this->valign[strtolower($options['valign'])]);
+            }
+
+            if (isset($options['border'])) {
+                $style->setBorder($this->setBorder(strtolower($options['border'])));
+            }
+            
+            if (isset($options['fill']) && $options['fill'] == 1) {
+                $style->setBackgroundColor($this->fillColor);
+            }
+        }
+
+        $this->cells[] = Cell::fromValue(value:$txt, style:$style);
+    }
+
+    public function rowEnd(){
+        $row = new Row($this->cells, $this->style);
+        $this->writer->addRow($row);
     }
 
     function close() {
         $this->writer->close();
     }
 
-    function open($fileName){
+    function openBrowser(string $fileName){
         $this->writer->openToBrowser($fileName);
     }
 
